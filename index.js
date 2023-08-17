b2ed5ba7220f3b837be46019d402e387057243f1
unknown <stephenoluwafemi52@gmail.comm>
2023-08-17 14:16:14 +0100

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+// Write you code here
 
-// Write your code here
+// const num1 = 2;
+// const num2 = 31;
+
+// const multiply = num1 * num2;
+
+// const random = Math.ceil(Math.random() * Number.MAX_SAFE_INTEGER);
+
+// const num3 = 8;
+// const num4 = 2;
+// const mod = num3 % num4;
+
+// const numbers = [10, 5, 20, 15, 8];
+// const max = Math.max(...numbers);
+
+
+
+
